5f75b179d2ff73ae00d0867a0a18673ee6bc4fd4
Lautaro Zarate <zlautaro43@gmail.com>
2024-11-20 12:11:32 -0300

    Agregando hook useNavigate

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,15 +1,20 @@
-import {Link} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 const Item = ({prod}) =>{
+    const navigate = useNavigate();
+    const handlerImageNavigate = () => {
+        navigate(`/item/${prod.id}`)
+    }
+    const handlerBtnNavigate = () => {
+        navigate(`/item/${prod.id}`)
+    }
     return(
         <div>
             <div>
-                <img src={prod.image} alt={prod.name} />
+                <img src={prod.image} alt={prod.name} onClick={handlerImageNavigate}/>
                 <div className="cards-description">
                     <h2>{prod.name}</h2>
                     <p>${prod.price}</p>
-                    <button><Link to={`/item/${prod.id}`}><img src="../public/icon.png" alt="icon"/></Link></button>
-                    {/* <Link><button><img src="./public/icon.png" alt="icon"/></button></Link> */}
-                    {/* <button><img src="./public/icon.png" alt="icon"/></button> */}
+                    <button><img src="./public/icon.png" alt="icon" onClick={handlerBtnNavigate}/></button>
                 </div>
             </div>
         </div>
