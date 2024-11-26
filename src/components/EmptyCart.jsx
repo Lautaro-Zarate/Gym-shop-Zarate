7f35f64e7b91579ef48d9b9d9107c21f03d83356
Lautaro Zarate <zlautaro43@gmail.com>
2024-11-26 17:53:38 -0300

    Estilando componente error y emptyCart

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom"
 const EmptyCart = () => {
     return( 
     <div>
-        <h1>Tu carrito está vacío</h1>
-        <img src="../public/emptycart.jpg" alt="Empty Cart" />
-        <h2>¿Que esperas? ¡Vé a comprar!</h2>
-        <Link to="/"><button>Ir al Inicio</button></Link>
+        <div className="primary-container">
+            <img src="../public/emptycart.jpg" alt="Empty Cart"/>
+            <h1>Tu carrito está vacío</h1>
+        </div>
+        <div className="secondary-container">
+            <h4>¿Que esperas? ¡Vé a comprar!</h4>
+            <Link to="/"><button>Ir al Inicio</button></Link>
+        </div>
     </div>
     )
 }
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
 const Error = () => {
     return(
-        <div>
-            <h1>Error de pagina</h1>
+        <div className="error-container">
+            <img src="../public/error.jpg" alt="Error Page" className="error-image"/>
+            <h3 className="error-title">¡Lo siento!</h3>
+            <h3 className="error-title">Esa página no existe</h3>
         </div>
     )
 }
